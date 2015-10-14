715b3c1a76bb0a9efeda48ca02e80593832626c3
Brad Burch <bradburch77@gmail.com>
2015-10-14 10:07:24 -0400

    Talked to Howard; Changing up Proj3

--- Compilers/ASTStmt.cpp
+++ Compilers/ASTStmt.cpp
@@ -7,3 +7,88 @@ using namespace std;
 ASTStmt::ASTStmt()
 {
 }
+
+ASTStmt * ASTStmt::assignNumStmt(string lhs, string num)
+{
+	return nullptr;
+}
+
+ASTStmt * ASTStmt::assignIDStmt(string lhs, string id)
+{
+	return nullptr;
+}
+
+ASTStmt * ASTStmt::assignOpStmt(string lhs, string id1, string op, string id2)
+{
+	return nullptr;
+}
+
+ASTStmt * ASTStmt::seqStmt(list<ASTStmt*> stmts)
+{
+	return nullptr;
+}
+
+ASTStmt * ASTStmt::whileStmt(string test, ASTStmt * body)
+{
+	return nullptr;
+}
+
+ASTStmt * ASTStmt::ifStmt(string test, ASTStmt * body)
+{
+	return nullptr;
+}
+
+ASTStmt * ASTStmt::untilStmt(string test, ASTStmt * body)
+{
+	return nullptr;
+}
+
+AssignID::AssignID(string l, string i)
+{
+}
+
+string AssignID::render(string indent)
+{
+	return string();
+}
+
+AssignNum::AssignNum(string l, string n)
+{
+}
+
+string AssignNum::render(string indent)
+{
+	return string();
+}
+
+IfStmt::IfStmt(string t, ASTStmt* b) {
+
+}
+
+string IfStmt::render(string indent)
+{
+	return string();
+}
+
+string Seq::render(string indent)
+{
+	return string();
+}
+
+Until::Until(string t, ASTStmt * b)
+{
+}
+
+string Until::render(string indent)
+{
+	return string();
+}
+
+While::While(string t, ASTStmt * b)
+{
+}
+
+string While::render(string indent)
+{
+	return string();
+}
